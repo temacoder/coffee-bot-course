872c96d086c480e812e3fd444d32a1e634c5d4e6
temacoder <temacoder@gmail.com>
2022-06-08 14:51:49 +0200

    Update for start project

--- src/handlers/callbackHandler.js
+++ src/handlers/callbackHandler.js
@@ -12,34 +12,5 @@ module.exports.CallbackHandler = (bot, db) => {
     }
 
 
-    if (callbackQuery.data.startsWith('espresso-')) {
-      const data = callbackQuery.data.split('-')
-      const weight = data[1]
-      const price = data[2]
-      await ctx.editMessageReplyMarkup()
-      await ctx.replyWithHTML(`Вы заказали кофе для эспрессо.\nВес - ${weight}гр\nЦена - ${price}руб`, InlineKeyboard.back())
-    }
-
-    if (callbackQuery.data.startsWith('filter-')) {
-      const data = callbackQuery.data.split('-')
-      const weight = data[1]
-      const price = data[2]
-      await ctx.editMessageReplyMarkup()
-      await ctx.replyWithHTML(`Вы заказали кофе для фильтра.\nВес - ${weight}гр\nЦена - ${price}руб`, InlineKeyboard.back())
-    }
-
-    if (callbackQuery.data === 'espresso') {
-      await ctx.deleteMessage()
-      await ctx.telegram.sendPhoto(ctx.chat.id, `${process.env.WEBHOOK}/espresso.png`, { caption: `Для эспрессо\n<b>${products['1'].title}</b>\n${products['1'].description}`, reply_markup: InlineKeyboard.coffee('1'), parse_mode: 'HTML'})
-    }
-
-    if (callbackQuery.data === 'filter') {
-      await ctx.deleteMessage()
-      await ctx.telegram.sendPhoto(ctx.chat.id, `${process.env.WEBHOOK}/filter.png`, { caption: `Для фильтра\n<b>${products['2'].title}</b>\n${products['2'].description}`, reply_markup: InlineKeyboard.coffee('2'), parse_mode: 'HTML'})
-    }
-
-
-
-
   })
 }
